b16f3da6db8d706caa6b042839a94da59a87da68
joshuagruenstein <joshuagruenstein@gmail.com>
2015-10-17 21:27:45 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+#include "ResourcePath.hpp"
+
+int main(int, char const**) {
+    sf::RenderWindow window(sf::VideoMode(1300, 1000), "Robby the Robot");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color(255,255,255));
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
